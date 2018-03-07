0bdf52cffffb9ec6f4457d7482b0b2ca942012fa
yangzhen10 <yangzhen10@staff.weibo.com>
2018-03-07 15:35:31 +0800

    add

---
+++ MainProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MainProject
+//
+//  Created by yangzhen10 on 2018/3/7.
+//  Copyright © 2018年 miko.yz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MainProject/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MainProject
+//
+//  Created by yangzhen10 on 2018/3/7.
+//  Copyright © 2018年 miko.yz. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
